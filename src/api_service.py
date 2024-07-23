ff7f7107e0d3429b0cc084587d700dbe432acbfd
Liudmila Beloritskaya <meiramirth@gmail.com>
2024-07-23 18:01:08 +0300

    check work

--- src/api_service.py
+++ src/api_service.py
@@ -4,6 +4,7 @@ from src.processing_vacancies import Vacancy
 import requests
 from accessify import private
 import os
+from dotenv import load_dotenv
 
 
 logger = logging.getLogger(__name__)
@@ -17,36 +18,44 @@ logger.setLevel(logging.INFO)
 
 
 class WebsiteAPI(ABC):
-    '''абстрактный класс для парсинга вакансий'''
+    """абстрактный класс для парсинга вакансий"""
+
     @abstractmethod
-    def get_vacancies(self, required_vacancy, required_salary, required_date):
+    def get_vacancies(self, required_vacancy, required_salary, required_period):
         pass
 
+
 class HeadHunterAPI(WebsiteAPI):
-    '''получение списка вакансий'''
+    """получение списка вакансий"""
 
     def __init__(self, BASE_URL):
         """получение информации о вакансиях"""
         logger.info("получение информации о вакансиях")
         self.base_url = BASE_URL
-        self.vacancies = []
+        # self.vacancies = []
 
-
-    def get_vacancies(self, required_vacancy, required_salary, required_date):
+    def get_vacancies(self, required_vacancy, required_salary, required_period):
 
         params = {
-                'per_page': 100,
-                'text': required_vacancy,
-                'salary': required_salary,
-                'only_with_salary': True,
-                'date_from': required_date
-            }
+            "per_page": 50,
+            "text": required_vacancy,
+            "only_with_salary": True,
+            "salary": required_salary,
+            "period": required_period,
+        }
         response = requests.get(url=self.base_url, params=params)
         if response.status_code != 200:
-                raise Exception(f"Ошибка запроса к API: Статус {response.status_code}")
+            raise Exception(f"Ошибка запроса к API: Статус {response.status_code}")
 
-        vacancies_list = response.json()['items']
+        vacancies_list = response.json()["items"]
         return vacancies_list
 
         # self.vacancies = Vacancy.cast_to_object_list(vacancies_list)
 
+
+if __name__ == "__main__":
+    load_dotenv()
+    MY_BASE_URL = os.getenv("BASE_URL")
+    hh_api = HeadHunterAPI(MY_BASE_URL)
+    hh_vacancies = hh_api.get_vacancies("Python", 100000, 3)
+    print(hh_vacancies)
