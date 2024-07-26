2dbf4df0bb0841703692defb3dc98d35109f98eb
Liudmila Beloritskaya <meiramirth@gmail.com>
2024-07-26 19:09:32 +0300

    add login and docstring in vacancy_manager

--- src/vacancy_manager.py
+++ src/vacancy_manager.py
@@ -15,9 +15,11 @@ logger.setLevel(logging.INFO)
 
 
 class Vacancy:
+    """Класс для представления вакансии"""
     def __init__(
         self, name_vacancy: str, city: str, url: str, salary: int = 0, currency: str = "", description: str = ""
     ):
+        """Инициализация вакансии и проверка данных"""
         self.name_vacancy = name_vacancy
         self.city = city
         self.url = url
@@ -27,7 +29,7 @@ class Vacancy:
         self._validate()
 
     def _validate(self):
-        """валидация данных"""
+        """Валидация данных вакансии"""
         if not self.name_vacancy:
             raise ValueError("Название вакансии не может быть пустым")
         if not self.url or not self.url.startswith("http"):
@@ -39,6 +41,8 @@ class Vacancy:
 
     @classmethod
     def from_api(cls, vacancy_data):
+        """Создает экземпляр Vacancy из данных API"""
+
         description = vacancy_data.get("snippet", {}).get("requirement", "Не указано")
         if not isinstance(description, str):
             description = "Не указано"
@@ -53,57 +57,73 @@ class Vacancy:
         )
 
     def __lt__(self, other):
+        """Сравнение вакансий по зарплате (меньше чем)"""
         if not isinstance(other, Vacancy):
             return NotImplemented
         return self.salary < other.salary
 
     def __le__(self, other):
+        """Сравнение вакансий по зарплате (меньше или равно)"""
         if not isinstance(other, Vacancy):
             return NotImplemented
         return self.salary <= other.salary
 
     def __eq__(self, other):
+        """Сравнение вакансий по зарплате (равно)"""
         if not isinstance(other, Vacancy):
             return NotImplemented
         return self.salary == other.salary
 
     def __ne__(self, other):
+        """Сравнение вакансий по зарплате (не равно)"""
         if not isinstance(other, Vacancy):
             return NotImplemented
         return self.salary != other.salary
 
     def __gt__(self, other):
+        """Сравнение вакансий по зарплате (больше чем)"""
         if not isinstance(other, Vacancy):
             return NotImplemented
         return self.salary > other.salary
 
     def __ge__(self, other):
+        """Сравнение вакансий по зарплате (больше или равно)"""
         if not isinstance(other, Vacancy):
             return NotImplemented
         return self.salary >= other.salary
 
     def __str__(self):
+        """Возвращает строковое представление вакансии"""
         return f"{self.name_vacancy} ({self.salary}, {self.currency}): {self.url}\nDescription: {self.description}"
 
 
 class VacancyManager:
+    """Класс для управления вакансиями"""
     def __init__(self, base_url: str):
+        """Инициализация менеджера вакансий"""
+        logger.info("Инициализация VacancyManager с базовым URL")
         self.api = HeadHunterAPI(base_url)
         self.vacancies = []
 
     def fetch_vacancies(self, query: str, salary: int, period: int):
+        """Получаем вакансии по запросу и фильтрует их по минимальной зарплате"""
+        logger.info(f"Получение вакансий по запросу: {query}, зарплата: {salary}, период: {period}")
         vacancies_data = self.api.get_vacancies(query, salary, period)
         self.vacancies = [
             Vacancy.from_api(vacancy)
             for vacancy in vacancies_data
             if vacancy.get("salary", {}).get("from") is not None and vacancy.get("salary", {}).get("from") >= salary
         ]
+        logger.info(f"Получено {len(self.vacancies)} вакансий.")
 
     def add_vacancy(self, vacancy: Vacancy):
+        """Добавляет вакансию в список"""
+        logger.info(f"Добавление вакансии: {vacancy}")
         self.vacancies.append(vacancy)
 
     def vacancies_by_keyword(self, keyword: str):
         """Фильтрует вакансии по ключевому слову в описании."""
+        logger.info(f"Фильтрация вакансий по ключевому слову: {keyword}"
         vacancies = [vacancy for vacancy in self.vacancies if keyword.lower() in vacancy.description.lower()]
         return vacancies
 
@@ -112,6 +132,7 @@ class VacancyManager:
         filtered_vacancies = self.vacancies_by_keyword(keyword)
         if not filtered_vacancies:
             print(f"Нет вакансий, содержащих ключевое слово '{keyword}' в описании.")
+            logger.info(f"Нет вакансий, содержащих ключевое слово '{keyword}' в описании.")
         return filtered_vacancies
 
 
